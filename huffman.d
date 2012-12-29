f2ed33b25d62837f607084be6d019e72e2634112
Marco Nembrini <sir.rainbow@gmail.com>
2012-12-29 20:12:35 +1100

    some additions.

--- huffman.d
+++ huffman.d
@@ -1,3 +1,27 @@
+import std.stdio;
+
+
+class Set(T) {
+
+    bool[T] dict;
+
+    this(T[] list) {
+        foreach(t; list){
+            dict[t] = true;
+        }
+    }
+
+    bool contains(T t){
+        auto p = t in dict;
+        if (p) { return true; }
+        else { return false; }
+    }
+
+    //alias dict this;
+}
+
+
+
 
 
 class Encoder {
@@ -10,26 +34,42 @@ class Encoder {
 
     }
     string encode(string data) {
-        analyze(data);
+        return "asd";
 
     }
 }
 
 class HuffmanEncoder : Encoder {
 
+    int[char] alphabet;
+    double[char] weights;
+    ulong data_len;
 
-    
     this() {
         super();
     }
 
-    string analyze(string data) {
+    void analyze(string data) {
         // first pass
+        data_len = data.length;
+        foreach(c; data) {
+            alphabet[c]++;
+        }
+        foreach(c; alphabet.byKey()){
+            weights[c] = cast(double)alphabet[c] / data_len;
+        }
     }
 }
 
 
 void main(string args[]) {
 
-    string data = "A quick brown fox jumps over the lazy something."
+    string data = "A quick brown fox jumps over the lazy something.";
+    bool[string] dict;
+    auto p = "asd" in dict;
+    writeln(p);
+    auto encoder = new HuffmanEncoder();
+    encoder.analyze("1234123adfasdf.");
+    writeln(encoder.weights);
+
 }
