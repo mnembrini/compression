23b82c717d1377492d825c3207cc3a029e01d5e6
Marco Nembrini <marco.nembrini.co@gmail.com>
2012-12-29 15:50:52 +1100

    started class encoder

---
+++ huffman.d
@@ -0,0 +1,35 @@
+
+
+class Encoder {
+    this() {
+        
+    }
+
+    void analyze(string data) {
+        // first pass
+
+    }
+    string encode(string data) {
+        analyze(data);
+
+    }
+}
+
+class HuffmanEncoder : Encoder {
+
+
+    
+    this() {
+        super();
+    }
+
+    string analyze(string data) {
+        // first pass
+    }
+}
+
+
+void main(string args[]) {
+
+    string data = "A quick brown fox jumps over the lazy something."
+}
